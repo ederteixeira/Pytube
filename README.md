1c6d2aee35c62bccda92cee97dc16ead791f4a4b
ederteixeira <ccbeder@gmail.com>
2021-12-30 21:47:21 -0300

    adicionando Gui

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# Pytube
+### Pytube
+
+#### Descrição
 
 Programa para baixar vídeos do Youtube.
 
+##### Ferramentas
+
 Utilizado a biblioteca Pytube.
 
-Página da documentação oficial:
+Instalação: 
+    $ pip3 install pytube
+
+Utilizado o Tkinter para geração da GUI.
+
+Documentação oficial:
 
     https://pytube.io/en/latest/
--- downyoutube.py
+++ downyoutube.py
@@ -1,12 +1,34 @@
 from pytube import YouTube
+from tkinter import *
 
-youtube = YouTube(input("Informe o link do video:"))
-diretorio = input("Informe o diretorio:  ")
- 
-print(f"Titulo: {youtube.title}.")
-print(f"Duração: {youtube.length} segundos.")
+def download():
+    youtube = YouTube(textLink.get())
+    diretorio = textDir.get()
+    diret= youtube.streams.get_highest_resolution() 
+    diret.download(diretorio)
+    status["text"] = 'Download completo!'
 
-diret= youtube.streams.get_highest_resolution() 
-print("Baixando!")
-diret.download(diretorio)
-print("Download completo")
+app = Tk()
+app.title("YoutubeDown")
+app.geometry("275x100")
+app.resizable(width=False, height=False)
+
+txtLink = Label(app, text="Link")
+txtLink.grid(column=0, row=0)
+
+textLink = Entry(app)
+textLink.grid(column=1, row=0)
+
+txtDir = Label(app, text="Pasta")
+txtDir.grid(column=0, row=1)
+
+textDir = Entry(app)
+textDir.grid(column=1, row=1)
+
+btn = Button(app, text="Baixar", command=download)
+btn.grid(column=4, row=0)
+
+status =  Label(app, text="")
+status.grid(column=1, row=2)
+
+app.mainloop()
